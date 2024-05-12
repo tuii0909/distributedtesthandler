47b06067f527d1e886f5f6d052bc741e6ac1c8eb
tuii0909 <fathovillepun0suy@hotmail.com>
2024-05-12 09:41:57 +0700

    feat task

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else right = mid;
-  }
-  return nums[left];
-}
+const lcmRecursive = (a, b) => {
+  return (a * b) / gcdRecursive(a, b);
+};
